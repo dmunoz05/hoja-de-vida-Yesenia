747652829fb083c8932b0c9dd73afc5971f56778
= <daniel20025febrero29@gmail.com>
2024-04-07 19:03:17 -0500

    Content for page

--- hoja-de-vida/index.js
+++ hoja-de-vida/index.js
@@ -4,17 +4,22 @@ window.addEventListener("DOMContentLoaded", () => {
   const DivContentTrasparent = document.querySelector('main.second div.div_content_second')
   const BtnsCloseMenu = document.querySelectorAll('button#closed-menu')
   const AllContent = document.querySelectorAll('div.div_content_second div.div_content')
+  const text = document.querySelector('.mi_hoja_vida_text')
 
   //Recorer todos los elementos del menu y agregarle un evento click.
   AllMenu.forEach((item) => {
     item.addEventListener("click", () => accessMenuSelected(item))
   })
 
+  //Cerrar menu
   BtnsCloseMenu.forEach((item) => {
     item.addEventListener("click", closeMenu)
   })
 
+  //Funcion para cerrar el menu
   function closeMenu() {
+    text.classList.toggle("display")
+    renderText(text, 200, 3000, undefined)
     AllContent.forEach((item) => {
       if(item.id !== ''){
         item.classList.add("display")
@@ -36,4 +41,33 @@ window.addEventListener("DOMContentLoaded", () => {
     contentShow.classList.remove("display")
   }
 
+
+  //Animacion en texto
+  function setDisplayText(text, html) {
+    html.classList.remove("display");
+    text.textContent = text;
+    html.textContent = text;
+  }
+
+  //Funcion para mostrar texto en pantalla con animación
+  function renderText(textHTML, interval, timeout, next) {
+    const originalText = textHTML.textContent;
+    text.textContent = '';
+
+    let currentIndex = 0;
+    const intervalId = setInterval(() => {
+      setDisplayText(originalText.substring(0, currentIndex + 1), textHTML);
+
+      currentIndex++;
+
+      if (currentIndex > originalText.length) {
+        clearInterval(intervalId);
+
+        setTimeout(() => {
+          next != undefined ? next() : null;
+        }, timeout)
+      }
+    }, interval);
+  }
+  renderText(text, 200, 3000, undefined)
 })
